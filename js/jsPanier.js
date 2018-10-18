c26f31aa98aca1cc14bfaef70d62be0aa4838ff1
codeurh24 <cci.corlouer@gmail.com>
2018-10-18 14:15:06 +0200

    avant de merge le master de fabien

--- js/jsPanier.js
+++ js/jsPanier.js
@@ -1,5 +1,7 @@
 // partie de florent qui gere le panier
 var tableauProduits = [];
+var PrixTotalPanier = 0;
+var quantiteProduitPanier
 $(function(){
 
   var addProduitDOMPanier = function(index, idProduit, title, qt, price){
@@ -25,6 +27,15 @@ $(function(){
 
     $(".modal-body .container-fluid").append(modalProduitElement);
   }
+  var paintPanier = function(){
+    $(".modal-body .container-fluid").html('');
+    window.tableauPanier.forEach(function(element, index) {
+      addProduitDOMPanier(index, element[0], element[1], element[2], element[3])
+      window.PrixTotalPanier  += Number(element[3]);
+    })
+  }
+
+
 
   window.tableauPanier = [
     [1, "gtx 960  GAMER", 2, "48.45"],
@@ -34,16 +45,32 @@ $(function(){
     [5, "Titant X geforce 1080", 2, "299.19"],
   ]
 
-  window.tableauPanier.forEach(function(element, index) {
-    addProduitDOMPanier(index, element[0], element[1], element[2], element[3])
-  })
+  //ajouterAuPanier(5, "Titant X geforce 1080", "299.19");
+  //idProduit, titreProduit, prixproduit
+  var ajouterAuPanier = function(){
+    window.valexist = false;
+    window.tableauPanier.forEach(function(element, index) {
+
+      var result = -1;
+      if( (result = jQuery.inArray(2,element)) != -1 ){
+        if( result == 0){
+          window.valexist = true;
+          console.log(window.valexist)
+        }
+      }
+    })
+
+  }
+  ajouterAuPanier();
+
+
+  paintPanier();
+  $("#nbrTotalProduitPanier").text(window.tableauPanier.length)
+  $("#priceTotalProduitPanier").text(window.PrixTotalPanier)
 
   $(".modal-body").on("click", ".supprimerProduit",function(){
     var idPanierDelete =  $(this).parent().parent().parent().attr('data-id-panier');
     delete window.tableauPanier[idPanierDelete];
-    $(this).parent().parent().parent().parent().html('');
-    window.tableauPanier.forEach(function(element, index) {
-      addProduitDOMPanier(index, element[0], element[1], element[2], element[3])
-    })
+    paintPanier();
   })
 });
